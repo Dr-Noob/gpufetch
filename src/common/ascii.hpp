7b2328589a5e17f567540fde897d71ebf6b739c3
Dr-Noob <peibolms@gmail.com>
2021-08-16 11:36:12 +0200

    [v0.03] Added different versions of long NVIDIA logo

--- src/common/ascii.hpp
+++ src/common/ascii.hpp
@@ -39,74 +39,6 @@ struct ascii_logo {
 
 // SHORT LOGOS //
 #define ASCII_NVIDIA \
-"$C1                   .#################.          \
-$C1              .####                   ####.     \
-$C1          .##                             ###   \
-$C1       ##                          :##     ###  \
-$C1    #                ##            :##      ##  \
-$C1  ##   ##  ######.   ####  ######  :##      ##  \
-$C1 ##    ##  ##:  ##:  ##   ##   ### :##     ###  \
-$C1##     ##  ##:  ##:  ##  :######## :##    ##    \
-$C1##     ##  ##:  ##:  ##   ##.   .  :## ####     \
-$C1##      #  ##:  ##:  ####  #####:   ##          \
-$C1 ##                                             \
-$C1  ###.                         ..o####.         \
-$C1   ######oo...         ..oo#######              \
-$C1          o###############o                     "
-
-// LONG LOGOS //
-#define ASCII_NVIDIA_L_V1 \
-"$C1               'cccccccccccccccccccccccccc  \
-$C1               ;oooooooooooooooooooooooool  \
-$C1           .:::.     .oooooooooooooooooool  \
-$C1      .:cll;   ,c:::.     cooooooooooooool  \
-$C1   ,clo'      ;.   oolc:     ooooooooooool  \
-$C1.cloo    ;cclo .      .olc.    coooooooool  \
-$C1oooo   :lo,    ;ll;    looc    :oooooooool  \
-$C1 oooc   ool.   ;oooc;clol    :looooooooool  \
-$C1  :ooc   ,ol;  ;oooooo.   .cloo;     loool  \
-$C1    ool;   .olc.       ,:lool        .lool  \
-$C1      ool:.    ,::::ccloo.        :clooool  \
-$C1         oolc::.            ':cclooooooool  \
-$C1               ;oooooooooooooooooooooooool  "
-
-#define ASCII_NVIDIA_L_V2 \
-"$C1               'cccccccccccccccccccccccccc  \
-$C1               ;oooooooooooooooooooooooool  \
-$C1           .:::.$C2;;;;;$C1.oooooooooooooooooool  \
-$C1      .:cll;$C2;;;$C1,c:::.$C2;;;;;$C1cooooooooooooool  \
-$C1   ,clo'$C2;;;;;;$C1;.$C2;;;$C1oolc:$C2;;;;;$C1ooooooooooool  \
-$C1.cloo$C2;;;;$C1;cclo$C2;$C1.$C2;;;;;;$C1.olc.$C2;;;;$C1coooooooool  \
-$C1oooo$C2;;;$C1:lo,$C2;;;;$C1;ll;$C2;;;;$C1looc$C2;;;;$C1:oooooooool  \
-$C1 oooc$C2;;;$C1ool.$C2;;;$C1;oooc;clol$C2;;;;$C1:looooooooool  \
-$C1  :ooc$C2;;;$C1,ol;$C2;;$C1;oooooo.$C2;;;$C1.cloo;$C2;;;;;$C1loool  \
-$C1    ool;$C2;;;$C1.olc.$C2;;;;;;;$C1,:lool$C2;;;;;;;;$C1.lool  \
-$C1      ool:.$C2;;;;$C1,::::ccloo.$C2;;;;;;;;$C1:clooool  \
-$C1         oolc::.$C2;;;;;;;;;;;;$C1':cclooooooool  \
-$C1               ;oooooooooooooooooooooooool  "
-
-#define ASCII_NVIDIA_L_V3 \
-"$C1               'cccccccccccccccccccccccccc  \
-$C1               ;oooooooooooooooooooooooool  \
-$C1           .:::.     .oooooooooooooooooool  \
-$C1      .:cll;   ,c:::.     cooooooooooooool  \
-$C1   ,clo'      ;.   oolc:     ooooooooooool  \
-$C1.cloo    ;cclo .      .olc.    coooooooool  \
-$C1oooo   :lo,    ;ll;    looc    :oooooooool  \
-$C1 oooc   ool.   ;oooc;clol    :looooooooool  \
-$C1  :ooc   ,ol;  ;oooooo.   .cloo;     loool  \
-$C1    ool;   .olc.       ,:lool        .lool  \
-$C1      ool:.    ,::::ccloo.        :clooool  \
-$C1         oolc::.            ':cclooooooool  \
-$C1               ;oooooooooooooooooooooooool  \
-$C1                                            \
-$C2#####   ##     ## ##  ######   ##    ###    \
-$C2##   ## ##     ## ##  ##    ## ##   ## ##   \
-$C2##   ##  ##   ##  ##  ##     # ##  ##   ##  \
-$C2##   ##   #####   ##  ##    ## ## ######### \
-$C2##   ##    ###    ##  ######   ## ##     ## "
-
-#define ASCII_NVIDIA_L \
 "$C1               'cccccccccccccccccccccccccc   \
 $C1               ;oooooooooooooooooooooooool   \
 $C1           .:::.     .oooooooooooooooooool   \
@@ -126,14 +58,80 @@ $C2######.  ##   ##  ##  ######   ##    ###     \
 $C2##   ##  ##   ##  ##  ##   ##  ##   #: :#    \
 $C2##   ##   ## ##   ##  ##   ##  ##  #######   \
 $C2##   ##    ###    ##  ######   ## ##     ##  "
+
+#define ASCII_NVIDIA_L_V1 \
+"$C1                      ####################################  \
+$C1                      ####################################  \
+$C1                      ####################################  \
+$C1                 #####          ##########################  \
+$C1            #######   ######        ######################  \
+$C1       ######         ###########      ###################  \
+$C1    #######      #####       #######     #################  \
+$C1  ######     #######  #         ######      ##############  \
+$C1#######    ######     #####      ######     ##############  \
+$C1  #####    #####      #####   #######     ################  \
+$C1  #####      ###      ############      ##################  \
+$C1    ######   ######   ##########      #######      #######  \
+$C1      #####    ##############      #######            ####  \
+$C1        #####      ###        #########           ########  \
+$C1         #######      #############           ############  \
+$C1            ###############              #################  \
+$C1                 #####           #########################  \
+$C1                      ####################################  \
+$C1                      ####################################  "
+
+#define ASCII_NVIDIA_L_V2 \
+"$C1                      ####################################  \
+$C1                      ####################################  \
+$C1                 #####          ##########################  \
+$C1           #########  ######        ######################  \
+$C1       ######         ###########      ###################  \
+$C1    #######       ####       #######     #################  \
+$C1 #######      ######  #         ######      ##############  \
+$C1#######    ######     #####      ######     ##############  \
+$C1 ######    #####      #####   #######     ################  \
+$C1  #####      ###      ############      ##################  \
+$C1    ######    #####   ##########      #######      #######  \
+$C1      #####     #############      #######            ####  \
+$C1        #####      ###        #########           ########  \
+$C1          ######      #############           ############  \
+$C1             #########                   #################  \
+$C1                  ####           #########################  \
+$C1                      ####################################  \
+$C1                      ####################################  \
+$C1                                                            \
+$C2 @@@@@@@@@   @@@    @@@  @@@  @@@@@@@@@   @@@       @@      \
+$C2 @@@    @@@  @@@    @@@  @@@  @@@    @@@  @@@      @@@@     \
+$C2 @@@    @@@   @@    @@   @@@  @@@     @@  @@@     @@  @@    \
+$C2 @@@    @@@    @@  @@    @@@  @@@     @@  @@@    @@    @@   \
+$C2 @@@    @@@    @@  @@    @@@  @@@    @@@  @@@   @@@@@@@@@@  \
+$C2 @@@    @@@     @@@@     @@@  @@@@@@@@@   @@@  @@@      @@@ "
+
+#define ASCII_NVIDIA_L \
+"$C1                  ##############################  \
+$C1                  ##############################  \
+$C1                ;:     '########################  \
+$C1          :cclloo ,:;.       ###################  \
+$C1      :cloc       :ooollcc:     '###############  \
+$C1   :cloc     ;:ccl       lolc,     #############  \
+$C1.cloo:    :clo.   ;c:      .ool;     ###########  \
+$C1 :ooo:   ,ooo     :ool,  .cloo.    ;l###########  \
+$C1  ,ooo:   ,ooc    :ooooccooo.    :####, o#######  \
+$C1    oooc.   ool:  :oooooo'    ,clooo       .####  \
+$C1     .ool:    .olc        .:clooo          :####  \
+$C1        oolc,     ;:::ccclooo.         :########  \
+$C1           oolcc::               ,:cclo#########  \
+$C1                 l      ;:::cclll###############  \
+$C1                  :lll##########################  "
+
 typedef struct ascii_logo asciiL;
 
 //                      -----------------------------------------------------------------------------------------------------
 //                      | LOGO          | W | H | REPLACE | COLORS LOGO (>0 && <10)        | COLORS TEXT (=2)               |
 //                      -----------------------------------------------------------------------------------------------------
-asciiL logo_nvidia    = { ASCII_NVIDIA,    48, 14, false, {COLOR_FG_CYAN},                 {COLOR_FG_CYAN, COLOR_FG_WHITE} };
+asciiL logo_nvidia    = { ASCII_NVIDIA,    45, 19, false, {COLOR_FG_GREEN, COLOR_FG_WHITE}, {COLOR_FG_WHITE, COLOR_FG_GREEN} };
 // Long variants        | --------------------------------------------------------------------------------------------------|
-asciiL logo_nvidia_l  = { ASCII_NVIDIA_L,  45, 19, false, {COLOR_FG_GREEN, COLOR_FG_WHITE}, {COLOR_FG_WHITE, COLOR_FG_GREEN} };
+asciiL logo_nvidia_l  = { ASCII_NVIDIA_L,  50, 15, false, {COLOR_FG_GREEN, COLOR_FG_WHITE}, {COLOR_FG_WHITE, COLOR_FG_GREEN} };
 asciiL logo_unknown   = { NULL,            0,  0,  false, {COLOR_NONE},                    {COLOR_NONE,    COLOR_NONE}     };
 
 #endif
